a08feb41f725b1fc0ac747839c0f4221ea5f04cc
Alex Klarfeld <alex.klarfeld@gmail.com>
2023-04-19 11:32:30 -0700

    Updated publish script added bump2version config

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 rm -rf dist
-https://raw.githubusercontent.com/supergoodsystems/docs/main/integrate-with-clients/python/README.md
+curl -C - -0 https://raw.githubusercontent.com/supergoodsystems/docs/main/integrate-with-clients/python/README.md > README.md
 python3 -m build
 python3 -m twine upload --repository pypi dist/*
