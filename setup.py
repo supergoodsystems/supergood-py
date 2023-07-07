0dda4170e84387a6438ddb9ceb8a71d80f03aa2e
Alex Klarfeld <alex.klarfeld@gmail.com>
2023-07-06 21:08:04 -0700

    Bump version: 1.0.16 → 1.0.17

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='supergood',
-    version='1.0.16',
+    version='1.0.17',
     author='Alex Klarfeld',
     description='The Python client for Supergood',
     long_description=long_description,
