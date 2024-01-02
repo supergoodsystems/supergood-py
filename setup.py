ebaf1067fa63ae09464376d06b88a35f18a563f9
Steve Bunting <steve@supergood.ai>
2024-01-02 13:04:31 -0800

    update version number to match pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="supergood",
-    version="1.0.21",
+    version="1.0.22",
     author="Alex Klarfeld",
     description="The Python client for Supergood",
     long_description=long_description,
