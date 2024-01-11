da30370ba31c3454f76ec103261e128f356adb76
Steve Bunting <steve@supergood.ai>
2024-01-11 14:35:40 -0800

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="supergood",
-    version="1.1.0",
+    version="1.1.1",
     author="Alex Klarfeld",
     description="The Python client for Supergood",
     long_description=long_description,
