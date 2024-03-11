8a987f4dc211ef4a94b06fc76869b142a467a2db
Steven Bunting <sbunting96@gmail.com>
2024-03-11 10:30:47 -0700

    support httpx streaming (#19)
    Co-authored-by: Steve Bunting <steve@supergood.ai>

--- src/supergood/vendors/httpx.py
+++ src/supergood/vendors/httpx.py
@@ -8,6 +8,7 @@ from ..constants import REQUEST_ID_KEY
 def patch(cache_request, cache_response):
     _original_handle_request = httpx.HTTPTransport.handle_request
     _original_response_read = httpx.Response.read
+    _original_response_iter_lines = httpx.Response.iter_lines
 
     _original_handle_async_request = httpx.AsyncHTTPTransport.handle_async_request
     _original_response_aread = httpx.Response.aread
@@ -42,21 +43,6 @@ def patch(cache_request, cache_response):
         setattr(response, REQUEST_ID_KEY, request_id)
         return response
 
-    def _wrap_response_read(response: httpx.Response):
-        response_body = _original_response_read(response)
-        request_id = getattr(response, REQUEST_ID_KEY)
-        status_text = response.extensions.get("reason_phrase", None)
-        if status_text:
-            status_text = status_text.decode("utf-8")
-        cache_response(
-            request_id,
-            response.text,
-            response.headers,
-            response.status_code,
-            status_text,
-        )
-        return response_body
-
     async def _wrap_response_aread(response: httpx.Response):
         response_body = await _original_response_aread(response)
         request_id = getattr(response, REQUEST_ID_KEY)
@@ -72,8 +58,43 @@ def patch(cache_request, cache_response):
         )
         return response_body
 
+    def _wrap_response_read(response: httpx.Response):
+        response_body = _original_response_read(response)
+        request_id = getattr(response, REQUEST_ID_KEY)
+        status_text = response.extensions.get("reason_phrase", None)
+        if status_text:
+            status_text = status_text.decode("utf-8")
+        cache_response(
+            request_id,
+            response.text,
+            response.headers,
+            response.status_code,
+            status_text,
+        )
+        return response_body
+
+    def _wrap_iter_lines(response: httpx.Response):
+        request_id = getattr(response, REQUEST_ID_KEY)
+        status_text = response.extensions.get("reason_phrase", None)
+        if status_text:
+            status_text = status_text.decode("utf-8")
+        response_parts = []
+        for line in _original_response_iter_lines(response):
+            if line:
+                response_parts.append(line)
+            yield line
+        response_body = "\n".join(response_parts)
+        cache_response(
+            request_id,
+            response_body,
+            response.headers,
+            response.status_code,
+            status_text,
+        )
+
     httpx.HTTPTransport.handle_request = _wrap_handle_request
     httpx.Response.read = _wrap_response_read
+    httpx.Response.iter_lines = _wrap_iter_lines
 
     httpx.AsyncHTTPTransport.handle_async_request = _wrap_handle_async_request
     httpx.Response.aread = _wrap_response_aread
