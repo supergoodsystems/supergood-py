d4d28f2d11160f2db10e5d9c29fea9221713976a
Steven Bunting <sbunting96@gmail.com>
2024-07-10 11:24:44 -0700

    Fix allowed keys for array-type keys (#37)
    * update version to 1.1.10
    
    * fix allowed keys for array types
    
    * add test
    
    ---------
    
    Co-authored-by: Steve Bunting <steve@supergood.ai>

--- tests/redaction/test_redact_by_default.py
+++ tests/redaction/test_redact_by_default.py
@@ -13,6 +13,7 @@ from tests.helper import get_config, get_remote_config
                 keys=[
                     ("responseBody.string", "ALLOW"),
                     ("responseBody.other_string", "REDACT"),
+                    ("responseBody[].data[].string", "ALLOW"),
                 ]
             ),
             "config": get_config(redact_by_default=True),
@@ -47,3 +48,42 @@ class TestRedactByDefault:
         assert filtered[0]["keyPath"] == "responseBody.other_string"
         assert filtered[0]["type"] == "string"
         assert filtered[0]["length"] == 3
+
+    # Test that allowed keys with array indexers are not redacted
+    def test_allowed_keys_of_arrays(self, httpserver, supergood_client):
+        httpserver.expect_request("/200").respond_with_json(
+            [
+                {
+                    "data": [
+                        {"string": "abc", "other_string": "sensitive"},
+                        {"string": "abc", "other_string": "sensitive"},
+                    ]
+                }
+            ]
+        )
+        requests.get(httpserver.url_for("/200"))
+        supergood_client.flush_cache()
+        args = Api.post_events.call_args[0][0]
+        response_body = args[0]["response"]["body"]
+        metadata = args[0]["metadata"]
+        assert len(response_body) == 1
+        assert len(response_body[0]["data"]) == 2
+        assert response_body[0]["data"][0]["string"] == "abc"
+        assert response_body[0]["data"][1]["string"] == "abc"
+
+        assert response_body[0]["data"][0]["other_string"] == None
+        assert response_body[0]["data"][1]["other_string"] == None
+
+        assert len(metadata["sensitiveKeys"]) > 0
+        # # There are a bunch of request/response headers. Filter for just responseBody
+        filtered = list(
+            filter(
+                lambda x: x["keyPath"].startswith("responseBody"),
+                metadata["sensitiveKeys"],
+            )
+        )
+        assert len(filtered) == 2
+        assert filtered[0]["keyPath"] == "responseBody[0].data[0].other_string"
+        assert filtered[1]["keyPath"] == "responseBody[0].data[1].other_string"
+        assert filtered[0]["type"] == "string" and filtered[1]["type"] == "string"
+        assert filtered[0]["length"] == 9 and filtered[1]["length"] == 9
